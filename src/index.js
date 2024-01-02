b61e25e1b1c6f22290dae30e62a0f6ff928e69e6
mohamedfawzi2 <mohamedfawzi20091996@gmail.com>
2024-01-02 03:39:48 +0200

    new update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.css';
-import Counter from './components/counter';
+import Counters from './components/counters';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode >
-    < Counter/>
+    < Counters/>
   </React.StrictMode>
 );
 

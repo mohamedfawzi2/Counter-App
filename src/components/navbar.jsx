0e7213f8b9d65313b03cbdd22dc47f89e307ae9b
mohamedfawzi2 <mohamedfawzi20091996@gmail.com>
2024-01-02 06:02:27 +0200

    final look

---
+++ src/components/navbar.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+const NavBar = ({totalCounters}) => {
+  return (
+      <nav class="navbar navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Navbar{" "}
+            <span className="badge badge-pill badge-secondary">
+              {totalCounters}
+            </span>
+          </a>
+        </div>
+      </nav>
+    );
+}
+
+
+export default NavBar;
